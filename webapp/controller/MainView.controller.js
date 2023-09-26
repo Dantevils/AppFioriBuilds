83f289718212262f4b13bf2de0da7864969bac56
Dantevils <dantes.alfredo@gmail.com>
2023-09-26 21:45:23 +0000

    New update 26.09.2023

--- webapp/controller/MainView.controller.js
+++ webapp/controller/MainView.controller.js
@@ -9,7 +9,23 @@ sap.ui.define([
 
         return Controller.extend("namespacefiori.fioriapp.controller.MainView", {
             onInit: function () {
+                //const oJSONMODEL = new sap.ui.model.json.JSONMODEL();
+                const oJSONModel = new sap.ui.model.json.JSONModel();
+                const oView = this.getView();
+                oJSONModel.loadData("../model/SelectionScreenMenu.json");
+                oView.setModel(oJSONModel, "selectionScreen");
+                console.log(oJSONModel);
+            },
 
+            onFilter: function(oEvent){
+            },
+            onClearFilter: function(){
+                const oModelSelScreen = this.getView().getModel("selectionScreen");
+                oModelSelScreen.setProperty("/ShipName","");
+                oModelSelScreen.setProperty("/CountryKey","");
+
+                //const oModelshipName = this.getView().getModel("shipName");
+                //oModelshipName.setProperty("shipName","");
             }
         });
     });
